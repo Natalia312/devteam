6e3a33da8c3d4a8d56bcff8907e8104e1f382438
Natalia312 <marchot@list.ru>
2019-12-21 13:33:01 +0400

    My Chart

--- assets/ChartjsAsset.php
+++ assets/ChartjsAsset.php
@@ -15,18 +15,18 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class AppAsset extends AssetBundle
+class ChartjsAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = '@app/companents/chartjs';
     public $css = [
-        'css/site.css',
+        'css/Chart.min.css',
     ];
     public $js = [
+        'js/Chart.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
-        'app\assets\ChartjsAsset',
     ];
+    
 }
---
+++ views/chartjs/index.php
@@ -0,0 +1,56 @@
+<?php
+$this->title = 'My first chart';
+
+$script = <<< JS
+ var speedCanvas = document.getElementById("speedChart");
+
+Chart.defaults.global.defaultFontFamily = "Lato";
+Chart.defaults.global.defaultFontSize = 18;
+
+var dataFirst = {
+    label: "Car A - Speed (mph)",
+    data: [0, 59, 75, 20, 20, 55, 40],
+    lineTension: 0,
+    fill: false,
+    borderColor: 'red'
+  };
+
+var dataSecond = {
+    label: "Car B - Speed (mph)",
+    data: [20, 15, 60, 60, 65, 30, 70],
+    lineTension: 0,
+    fill: false,
+  borderColor: 'blue'
+  };
+
+var speedData = {
+  labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
+  datasets: [dataFirst, dataSecond]
+};
+
+var chartOptions = {
+  legend: {
+    display: true,
+    position: 'top',
+    labels: {
+      boxWidth: 80,
+      fontColor: 'black'
+    }
+  }
+};
+
+var lineChart = new Chart(speedCanvas, {
+  type: 'line',
+  data: speedData,
+  options: chartOptions
+});
+
+JS;
+$this->registerJs($script, yii\web\View::POS_READY);
+?>
+
+<div id="my-chart">
+	<h1><?= $this->title ?></h1>
+
+	<canvas id="speedChart" width="600" height="400"></canvas>
+</div>
